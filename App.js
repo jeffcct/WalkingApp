73d0186b97fb455eb2cb8fa6af5497bd291243f4
alex <alexabelita@gmail.com>
2024-08-31 15:59:16 +1200

    added header

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 // Import the screen components
-import HomeScreen from './Screens/HomeScreen';
-import HistoryScreen from './Screens/HistoryScreen';
-import ChallengesScreen from './Screens/ChallengesScreen';
-import ProfileScreen from './Screens/ProfileScreen';
+import HomeScreen from "./Screens/HomeScreen";
+import HistoryScreen from "./Screens/HistoryScreen";
+import ChallengesScreen from "./Screens/ChallengesScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }} // Hide the default header
+      />
       <Tab.Screen name="History" component={HistoryScreen} />
       <Tab.Screen name="Challenges" component={ChallengesScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
