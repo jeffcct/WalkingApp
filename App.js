bd6bd063bb18c04e023e7dd11c7b1cc329f89af4
jeffcct <jsmi607@aucklanduni.ac.nz>
2024-09-01 10:09:33 +1200

    Gets location through expo-location

--- App.js
+++ App.js
@@ -1,11 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import MapView, { Marker, Region } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [beginningLocation, setBLocation] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+
+
+  const setStartPosition = () => {
+    useEffect(() => {
+      (async () => {
+        
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Permission to access location was denied');
+          return;
+        }
+  
+        let location = await Location.getCurrentPositionAsync({});
+        setBLocation(location);
+      })();
+    }, []);
+  };
+
+  const toggleRun = () => {
+    setIsRunning(!isRunning);
+    setStartPosition();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        provider={MapView.PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+        />
+      </MapView>
+
+      <TouchableOpacity
+        style={isRunning ? styles.stopButton : styles.startButton}
+        onPress={toggleRun}
+      >
+        <Text style={styles.buttonText}>{isRunning ? "Stop Run" : "Start Run"}</Text>
+      </TouchableOpacity>
+      <Button style="styles.button" onPress={setStartPosition} title={"Check Distance"}>
+        <Text style={styles.buttonText}>Check Distance</Text>
+      </Button>
+      <View>
+        <Text>
+          {JSON.stringify(location)}
+        </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +84,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '90%',
+    height: '50%',
+    marginBottom: 20,
+  },
+  startButton: {
+    backgroundColor: 'green',
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    alignItems: 'center',
+  },
+  stopButton: {
+    backgroundColor: 'red',
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
